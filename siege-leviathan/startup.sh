9e6b7e3689d3cb7dfa996bb3111515520bcd2ca1
scholar-of-artifice <206404745+scholar-of-artifice@users.noreply.github.com>
2025-12-11 18:44:45 +0000

    create a startup script. this is nearly identical as for other services

---
+++ siege-leviathan/startup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# start vault agent in the background
+echo "🕵️ starting Vault agent"
+
+vault agent -config=agent-config.hcl -exit-after-auth=false &
+
+echo "⏱️ waiting on certificates"
+until [ -f /app/certs/ca.crt ] && [ -f /app/certs/siege-leviathan.crt ] && [ -f /app/certs/siege-leviathan.key ]; do 
+    echo "..."
+    sleep 1
+done
+
+echo "✅ complete: certificates found!"
+
+echo "🚀 starting siege-leviathan"
+
+exec uvicorn main:app --host 0.0.0.0 --port 8003
