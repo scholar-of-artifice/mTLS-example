dd126516f6c64b6a0ae9c3666c57e80d6f0e419b
scholar-of-artifice <206404745+scholar-of-artifice@users.noreply.github.com>
2025-12-11 19:16:35 +0000

    basic fastAPI app to run

--- siege-leviathan/main.py
+++ siege-leviathan/main.py
@@ -1,6 +1,47 @@
-def main():
-    print("Hello from siege-leviathan!")
+import ssl
+import httpx
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# define where the Vault agent will place the certs
+CA_CERT = "/app/certs/ca.crt"
+CLIENT_CERT = "/app/certs/siege-leviathan.crt"
+CLIENT_KEY = "/app/certs/siege-leviathan.key"
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+async def root():
+    """
+    This endpoint triggers the interaction.
+    It acts as a Client to the 'overwhelming-minotaur' service.
+    """
+    # create a secture SLL context
+    ssl_context = ssl.create_default_context(
+        purpose=ssl.Purpose.SERVER_AUTH,
+        cafile=CA_CERT
+    )
+    # load the client certificate and private key to prove OUR identity
+    ssl_context.load_cert_chain(
+        certfile=CLIENT_CERT,
+        keyfile=CLIENT_KEY
+    )
+    # define the tart service URL
+    # NOTE: this comes from the docker-compose
+    target_url = "https://overwhelming-minotaur:9000"
+    # send the message defined as an example
+    message = "I sent you a secret message *giggle*"
+    print(f"Attempting to contact {target_url}...")
+    try:
+        # use httpx with the custom SSL context
+        async with httpx.AsyncClient(verify=ssl_context) as client:
+            response = await client.post(target_url, content=message)
+            # return the conversation log
+            return {
+                "siege_leviathan_says": message,
+                "overwhelming_minotaur_responds": response.text
+            }
+    except Exception as e:
+        return {
+            "error": f"Failed to connect to Minotaur: {str(e)}"
+        }
